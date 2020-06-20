fb40c99ae3befe81b81bb43314e70e67be387ff2
Parker Williamson <parker.williamson@gmail.com>
2020-06-20 23:51:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/team' do
+      @team = Team.new(params[:team])
+        params[:team][:members].each {|details| SuperHero.new(details)}
+        @super_heroes = SuperHero.all
+
+        erb :team
+    end
 end
